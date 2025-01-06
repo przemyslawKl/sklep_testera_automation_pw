004db839bf5c6f4a4344da6037cbf84c0fa3f530
przemyslawKl <klobus.p@gmail.com>
2025-01-06 22:40:41 +0100

    Changes: - changed Properties.java - changed application.properties - changes in BaseTest.java

--- src/main/java/org/example/utils/Properties.java
+++ src/main/java/org/example/utils/Properties.java
@@ -11,4 +11,7 @@ public class Properties {
     public static String getProperty (String propertyName) {
         return ResourceBundle.getBundle("application").getString(propertyName);
     }
+    private static int getViewportDimension(String propertyName) {
+        return Integer.parseInt(getProperty(propertyName));
+    }
 }
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import com.microsoft.playwright.*;
+import org.example.utils.Properties;
 import org.junit.jupiter.api.*;
 
 import java.nio.file.Paths;
@@ -22,34 +23,45 @@ public class BaseTest {
     static void launchBrowser() {
         pw = Playwright.create();
         browser = pw.chromium().launch(new BrowserType.LaunchOptions()
-                .setHeadless(false));
+                .setHeadless(Boolean.parseBoolean(Properties.getProperty("browser.headless")))
+                .setSlowMo(Integer.parseInt(Properties.getProperty("browser.slow.motion")))
+                .setChannel(Properties.getProperty("browser.channel")));
     }
 
 
     @BeforeEach
     void createContextandPage() {
         context = browser.newContext();
+
+        if(isTracingEnabled()){
         context.tracing().start(new Tracing.StartOptions()
                 .setScreenshots(true)
                 .setSnapshots(true)
-                .setSources(true)
-        );
+                .setSources(true));
+        }
         page = context.newPage();
-        page.setViewportSize(1920, 1080);
+        page.setViewportSize(Integer.parseInt(Properties.getProperty("browser.width")),
+                Integer.parseInt(Properties.getProperty("browser.height")));;
     }
 
     @AfterEach
     void closeContext(TestInfo testInfo) {
-        String traceName = "traces/trace_"
-                + removeRoundBrackets(testInfo.getDisplayName())
-                + "_" + LocalDateTime.now().format(DateTimeFormatter
-                .ofPattern("yyyy-MM-dd_HH-mm-ss")) + ".zip";
-        context.tracing().stop(new Tracing.StopOptions().setPath(Paths.get(traceName)));
+        if(isTracingEnabled()) {
+            String traceName = "traces/trace_"
+                    + removeRoundBrackets(testInfo.getDisplayName())
+                    + "_" + LocalDateTime.now().format(DateTimeFormatter
+                    .ofPattern(Properties.getProperty("tracing.date.format"))) + ".zip";
+            context.tracing().stop(new Tracing.StopOptions().setPath(Paths.get(traceName)));
+        }
         context.close();
     }
 
     @AfterAll
     static void closeBrowser() {
         pw.close();
+    }
+
+    private boolean isTracingEnabled() {
+        return Boolean.parseBoolean(Properties.getProperty("tracing.enabled"));
     }
 }
