5640bac84bfc1eb609c6a5f72000190ea740a65d
przemyslawKl <klobus.p@gmail.com>
2025-01-07 22:05:06 +0100

    Changes: - change in HomePage.java, SearchTest.java and TopMenuWithSearchSection.java - added TopNavigationSection.java

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -3,16 +3,19 @@ package pages;
 import com.microsoft.playwright.Page;
 import lombok.Getter;
 import sections.TopMenuWithSearchSection;
+import sections.TopNavigationSection;
 
+@Getter
 public class HomePage {
 
     protected Page page;
-    @Getter
     private TopMenuWithSearchSection topMenuWithSearchSection;
+    private TopNavigationSection topNavigationSection;
 
     public HomePage (Page page){
         this.page = page;
         this.topMenuWithSearchSection = new TopMenuWithSearchSection(page);
+        this.topNavigationSection = new TopNavigationSection(page);
     }
 
 }
